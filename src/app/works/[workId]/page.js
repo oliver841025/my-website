6f6f440732767fbe322dcd52cd7e2c01b0c364fd
ChingHsuan <hsuan.huang@kickscrew.com>
2023-07-31 18:32:27 +0800

    style: folder structure

---
+++ src/app/works/[workId]/page.js
@@ -0,0 +1,6 @@
+export default function WorkDetail({params}){
+    const {workId} = params;
+    return (
+        <h1>Work Detail {workId}</h1>
+    )
+}
---
+++ src/app/works/page.js
@@ -0,0 +1,5 @@
+export default function Works(){
+    return (
+        <h1>Works main page</h1>
+    )
+}
