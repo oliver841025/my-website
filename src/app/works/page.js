ba0ad7944e455a298ebf23693866d693b24e62b3
ChingHsuan <hsuan.huang@kickscrew.com>
2023-08-03 17:59:53 +0800

    docs: add prettier docs and settings

--- src/app/works/page.js
+++ src/app/works/page.js
@@ -1,5 +1,9 @@
-export default function Works(){
-    return (
-        <h1>Works main page</h1>
-    )
+export default function Works() {
+  return (
+    <>
+      <div>
+        <p>asd</p>
+      </div>
+    </>
+  );
 }
