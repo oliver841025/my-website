553017fe2f965183802830e821430a7da92ec5cd
ChingHsuan <hsuan.huang@kickscrew.com>
2023-08-03 20:09:41 +0800

    feat: header + global

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,6 @@
-import myHeader from '../components/header/header';
-
 export default function Home() {
   return (
     <>
-      <myHeader />
       <h1>Home</h1>
     </>
   );
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,10 +3,12 @@ import classes from './header.module.scss';
 export default function Header() {
   return (
     <>
-      <section className={classes.wrapper}>
-        <div>WORK</div>
-        <div>INFO</div>
-      </section>
+      <div className={classes.whole_wrapper}>
+        <section className={classes.left_wrapper}>
+          <div className={classes.work_btn}>WORK</div>
+          <div className={classes.info_btn}>INFO</div>
+        </section>
+      </div>
     </>
   );
 }
