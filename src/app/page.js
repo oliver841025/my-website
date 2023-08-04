4c2245cc1e187be36e9cdfa52b3f52a4a85f4e2e
ChingHsuan <hsuan.huang@kickscrew.com>
2023-08-04 18:28:09 +0800

    feat: header button filter

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+import styles from './style.module.scss';
+
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div>
+        <h1>Home</h1>
+        <p className={styles.whole_wrapper}>
+          Available 24 hours, maybe we can have a chance to talk (☞ﾟヮﾟ)☞
+        </p>
+      </div>
     </>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,45 @@
+'use client';
+import { useState } from 'react';
 import classes from './header.module.scss';
 
-export default function Header() {
+export default function Header(props) {
+  const { filter, setFilter } = props;
+
+  const [workClicked, setWorkClicked] = useState(false);
+  const [infoClicked, setInfoClicked] = useState(false);
+
+  const handleClick = (param) => {
+    setFilter(param);
+
+    if (param === 'work') {
+      setWorkClicked(true);
+      setInfoClicked(false);
+    } else if (param === 'info') {
+      setWorkClicked(false);
+      setInfoClicked(true);
+    }
+  };
+
   return (
     <>
       <div className={classes.whole_wrapper}>
         <section className={classes.left_wrapper}>
-          <div className={classes.work_btn}>WORK</div>
-          <div className={classes.info_btn}>INFO</div>
+          <div
+            className={`${classes.work_btn} ${workClicked ? classes.clicked : ''}`}
+            onClick={() => {
+              handleClick('work');
+            }}
+          >
+            WORK
+          </div>
+          <div
+            className={`${classes.info_btn} ${infoClicked ? classes.clicked : ''}`}
+            onClick={() => {
+              handleClick('info');
+            }}
+          >
+            INFO
+          </div>
         </section>
       </div>
     </>
