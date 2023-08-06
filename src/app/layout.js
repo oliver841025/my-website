f295065c4b8e639e55f3a9c605f562fae20ca0b3
ChingHsuan <hsuan.huang@kickscrew.com>
2023-08-06 16:59:50 +0800

    style: golbal.css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
 'use client';
 import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
-import styles from '../styles/global.module.scss';
+import '../styles/global.css';
 import { useState } from 'react';
 
 export default function RootLayout({ children }) {
   const [filter, setFilter] = useState(null);
   return (
     <html lang="en">
-      <body className={styles.body}>
+      <body>
         <Header filter={filter} setFilter={setFilter} />
         {children}
         <Footer />
