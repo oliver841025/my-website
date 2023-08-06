417aa1ef3f8326bd537ea20c8e682cce95098db0
ChingHsuan <hsuan.huang@kickscrew.com>
2023-08-06 17:49:49 +0800

    homepage layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
-import styles from './style.module.scss';
+import classes from './style.module.scss';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>Home</h1>
-        <p className={styles.whole_wrapper}>
-          Available 24 hours, maybe we can have a chance to talk (☞ﾟヮﾟ)☞
+      <div className={classes.wrapper}>
+        <Image src="/doggy.jpeg" width={350} height={350} alt="Picture of the author" />
+        <div className={classes.author}>OLIVER HUANG</div>
+      </div>
+      <div className={classes.recommendation_wrapper}>
+        <p id="recommendation-text" className={classes.recommendation}>
+          Available 24 hours, contact me (☞ﾟヮﾟ)☞ Available 24 hours, contact me (☞ﾟヮﾟ)☞ 
         </p>
       </div>
     </>
