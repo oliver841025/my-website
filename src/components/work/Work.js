d0848d9733dd2bc5482e44e199015f87797a964c
ChingHsuan <hsuan.huang@kickscrew.com>
2023-08-10 18:15:44 +0800

    feat: work card layout

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -4,7 +4,18 @@ import Image from 'next/image';
 export default function Info() {
   return (
     <>
-      <div className={classes.wrapper}>work card</div>
+      <div className={classes.wrapper}>
+        <section className={classes.top_wrapper}>
+          <div className={`${classes.filter} ${classes.website}`}>WEBSITE</div>
+          <div className={`${classes.filter} ${classes.video}`}>VIDEO</div>
+          <div className={`${classes.filter} ${classes.graphic}`}>GRAPHIC</div>
+        </section>
+        <section className={classes.bottom_wrapper}>
+          <Image src="/doggy.jpeg" width={300} height={300} alt="Picture of the author" />
+          <Image src="/doggy.jpeg" width={300} height={300} alt="Picture of the author" />
+          <Image src="/doggy.jpeg" width={300} height={300} alt="Picture of the author" />
+        </section>
+      </div>
     </>
   );
 }
