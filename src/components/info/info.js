9aa76c67c6098205ff30a41f871f84166005ef3e
ChingHsuan <hsuan.huang@kickscrew.com>
2023-08-10 17:42:02 +0800

    style: info card layout

--- src/components/info/info.js
+++ src/components/info/info.js
@@ -1,24 +1,32 @@
 import classes from './info.module.scss';
+import Image from 'next/image';
 
 export default function Info() {
   return (
     <>
       <div className={classes.wrapper}>
-        <h2>OLIVER HUANG</h2>
+        <h2 className={classes.author}>OLIVER HUANG</h2>
         <section>
-          <p>
+          <p className={classes.about}>
             something about me something about me something about me something about me something
-            about me
+            about me something about me something about me something about me something about me
+            something about me something about me something about me something about me something
+            about me something about me something about me something about me something about me
+            something about me something about me something about me something about me something
+            about me something about me something about me something about me something about me
+            something about me something about me something about me
           </p>
-          {/* <Image src="/doggy.jpeg" width={350} height={350} alt="Picture of the author" /> */}
+          <div className={classes.image_wrapper}>
+            <Image src="/doggy.jpeg" width={300} height={300} alt="Picture of the author" />
+          </div>
         </section>
         <section>
-          <h3>EXPERIENCE</h3>
-          <div>
+          <h3 className={classes.experience_title}>EXPERIENCE</h3>
+          <div className={classes.experience_content}>
             <div>2023 / 05 - now</div>
             <div>KICKSCREW Front-end Engineer</div>
           </div>
-          <div>
+          <div className={classes.experience_content}>
             <div>2023 / 05 - now</div>
             <div>KICKSCREW Front-end Engineer</div>
           </div>
