b07105c38a0fad79a1f5e5bba838d00a115fe941
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-08-13 16:21:27 +0800

    add website filter

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,16 +1,33 @@
+'use client';
 import classes from './work.module.scss';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Info() {
+  const [cardFilter, setCardFilter] = useState(null);
+
+  const handleClick = (param) => {
+    setCardFilter(param);
+  };
+
   return (
     <>
       <div className={classes.wrapper}>
         <section className={classes.top_wrapper}>
-          <div className={`${classes.filter} ${classes.website}`}>WEBSITE</div>
-          <div className={`${classes.filter} ${classes.video}`}>VIDEO</div>
-          <div className={`${classes.filter} ${classes.graphic}`}>GRAPHIC</div>
+          <div className={`${classes.filter} ${classes.website}`} onClick={handleClick('website')}>
+            WEBSITE
+          </div>
+          <div className={`${classes.filter} ${classes.video}`} onClick={handleClick('video')}>
+            VIDEO
+          </div>
+          <div className={`${classes.filter} ${classes.graphic}`} onClick={handleClick('graphic')}>
+            GRAPHIC
+          </div>
         </section>
         <section className={classes.bottom_wrapper}>
+          {cardFilter === 'website'}
+          {cardFilter === 'video'}
+          {cardFilter === 'graphic'}
           <Image src="/doggy.jpeg" width={300} height={300} alt="Picture of the author" />
           <Image src="/doggy.jpeg" width={300} height={300} alt="Picture of the author" />
           <Image src="/doggy.jpeg" width={300} height={300} alt="Picture of the author" />
