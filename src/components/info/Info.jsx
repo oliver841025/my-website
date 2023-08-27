03b0fc784ef8274e172ef4ec34782c5c3578433c
ChingHsuan <hsuan.huang@kickscrew.com>
2023-08-27 15:20:50 +0800

    feat: fetch sanity ing

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,16 +1,18 @@
 import classes from './info.module.scss';
 import Image from 'next/image';
+import { createClient } from 'next-sanity';
 
-export default function Info() {
+export default function Info({ names }) {
+  console.log('names', names);
   return (
     <>
       <div className={classes.wrapper}>
-        <h2 className={classes.author}>OLIVER HUANG</h2>
+        <h2 className={classes.author}>{names?.name}</h2>
         <section>
           <p className={classes.about}>
-            currently a frontend engineer who enjoys exploring technology. I appreciate maintaining
-            flexibility in life and gravitate towards possibilities-rich endeavors. I also have a
-            fondness for indulging in a delicious matcha latte.
+            currently a frontend engineer who enjoys exploring technology. I appreciate maintaining flexibility in life
+            and gravitate towards possibilities-rich endeavors. I also have a fondness for indulging in a delicious
+            matcha latte.
           </p>
           <div className={classes.image_wrapper}>
             <Image src="/doggy.jpeg" width={300} height={300} alt="Picture of the author" />
@@ -30,4 +32,22 @@ export default function Info() {
       </div>
     </>
   );
+}
+
+const client = createClient({
+  projectId: 'hj4awfcj',
+  dataset: 'production',
+  apiVersion: '2023-08-27',
+  useCdn: false,
+});
+
+export async function getStaticProps() {
+  const names = await client.fetch(`*[_type == "info"]`);
+  console.log('names', names);
+
+  return {
+    props: {
+      names,
+    },
+  };
 }
