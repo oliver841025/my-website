9f61f0c202e70c5b85b6f8ede969d5a2b72bd0c4
ChingHsuan <hsuan.huang@kickscrew.com>
2023-09-15 00:50:49 +0800

    feat: cover

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     domains: ['cdn.sanity.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        // port: '',
+        pathname: '/images/**',
+      },
+    ],
   },
 };
 
--- src/components/website/Website.jsx
+++ src/components/website/Website.jsx
@@ -8,9 +8,10 @@ export default function Website(props) {
   return (
     <>
       <div className={classes.website}>
+        <div className={classes.cover}>
+          <h4 className={classes.title}>{websiteData[2].name}</h4>
+        </div>
         <Image src={websiteData[2].imgUrl} width={400} height={230} alt="KICKSCREW" />
-        <h4>{websiteData[2].name}</h4>
-        <p>{websiteData[2].description}</p>
       </div>
     </>
   );
