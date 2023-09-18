139feca404d1d12c4a7f083e94c0febb5c194e7c
ChingHsuan <hsuan.huang@kickscrew.com>
2023-09-18 23:59:56 +0800

    fix; reopen image in website

--- src/components/website/Website.jsx
+++ src/components/website/Website.jsx
@@ -12,7 +12,7 @@ export default function Website(props) {
           <div className={classes.cover}>
             <h4 className={classes.title}>{item.name}</h4>
           </div>
-          {/* <Image src={item.imgUrl} width={400} height={230} alt={item.name} /> */}
+          <Image src={item.imgUrl} width={400} height={230} alt={item.name} />
         </div>
       ))}
     </>
