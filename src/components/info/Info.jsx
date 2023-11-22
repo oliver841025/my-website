e70634ea61d71fe8e42f2261fe9616fa597a618d
ChingHsuan <hsuan.huang@kickscrew.com>
2023-11-22 23:31:18 +0800

    delete shit searchParams in info

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,24 +1,10 @@
 'use client';
-import { useEffect, useState } from 'react';
 import classes from './info.module.scss';
 import Image from 'next/image';
-import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Info(props) {
   const { infoData } = props;
-  const [filter, setFilter] = useState(null);
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const [isCross, setIsCross] = useState(false);
 
-  useEffect(() => {
-    const targetSearchParams = searchParams.toString();
-    if (targetSearchParams === 'filter=info') {
-      setIsCross(true);
-    } else {
-      setIsCross(false);
-    }
-  }, [pathname, searchParams]);
 
   return (
     <>
