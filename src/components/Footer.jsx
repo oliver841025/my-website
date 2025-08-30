d5a6fb2f50fce4d69784882a39770a54796daf6c
ChingHsuan <hsuan.huang@kickscrew.com>
2025-08-30 13:07:35 +0800

    fix: footer balck

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4 mt-20">
+    <footer className="footer sm:footer-horizontal text-neutral-content items-center p-4 mt-20">
       <aside className="flex items-center justify-center text-sm">
         {/* <p>Copyright © {new Date().getFullYear()} - All right reserved</p> */}
       </aside>
