7736f16732e436579dd495bd7a0d2adc0e78e639
ChingHsuan <hsuan.huang@kickscrew.com>
2025-08-30 13:09:54 +0800

    fix: rwd header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,48 +3,48 @@ import { useTranslations } from 'next-intl';
 import { Link } from '@/i18n/routing';
 
 import LanguageSwitcher from './LanguageSwitcher';
-import Image from 'next/image';
 
 export default function Header() {
   const t = useTranslations('header');
 
   return (
     <div
-      className=" font-bold italic grid grid-cols-[auto_1fr_auto_auto] items-center -mt-4 sticky top-0 z-50"
+      className="font-bold italic flex items-center justify-between -mt-4 sticky top-0 z-50"
       style={{ minHeight: 64 }}
     >
       <div className="hover:text-blue-800">
         <a
           href="/"
-          className="btn btn-ghost text-xl  hover:outline-none focus:outline-none shadow-none border-none font-mono"
+          className="btn btn-ghost text-xl hover:outline-none focus:outline-none shadow-none border-none font-mono"
         >
           HSUANSKY
         </a>
       </div>
-      <div /> {/* 空白區域撐開 */}
-      <div className="justify-self-end mr-8">
-        <LanguageSwitcher />
+      <div className="flex items-center gap-8">
+        <nav className="flex space-x-4">
+          <Link
+            href="/about"
+            className="btn btn-ghost hover:outline-none hover:text-blue-800 focus:outline-none shadow-none border-none"
+          >
+            {t('me')}
+          </Link>
+          <Link
+            href="/works"
+            className="btn btn-ghost hover:outline-none hover:text-blue-800 focus:outline-none shadow-none border-none"
+          >
+            {t('works')}
+          </Link>
+          <Link
+            href="/articles"
+            className="btn btn-ghost hover:outline-none hover:text-blue-800 focus:outline-none shadow-none border-none"
+          >
+            {t('articles')}
+          </Link>
+        </nav>
+        <div className="ml-4">
+          <LanguageSwitcher />
+        </div>
       </div>
-      <nav className="space-x-4 w-40 justify-self-end">
-        <Link
-          href="/about"
-          className="btn btn-ghost  hover:outline-none hover:text-blue-800 focus:outline-none shadow-none border-none"
-        >
-          {t('me')}
-        </Link>
-        <Link
-          href="/works"
-          className="btn btn-ghost  hover:outline-none hover:text-blue-800 focus:outline-none shadow-none border-none"
-        >
-          {t('works')}
-        </Link>
-        <Link
-          href="/articles"
-          className="btn btn-ghost  hover:outline-none hover:text-blue-800 focus:outline-none shadow-none border-none"
-        >
-          {t('articles')}
-        </Link>
-      </nav>
     </div>
   );
 }
