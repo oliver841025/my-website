e30bfe10b90068da20c2bfd1d41af6af6799fd08
ChingHsuan <hsuan.huang@kickscrew.com>
2025-09-03 01:25:46 +0800

    fix: remove footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,3 @@
 export default function Footer() {
-  return (
-    <footer className="footer sm:footer-horizontal text-neutral-content items-center p-4 mt-20">
-      <aside className="flex items-center justify-center text-sm">
-        {/* <p>Copyright © {new Date().getFullYear()} - All right reserved</p> */}
-      </aside>
-    </footer>
-  );
+  return <footer className=""></footer>;
 }
