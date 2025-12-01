749e27eb42af6042163df6adc4d43c1c1ea3d19a
ChingHsuan <hsuan.huang@kickscrew.com>
2025-12-01 23:21:10 +0800

    fix: shorten daisy intro

--- src/constants/works.js
+++ src/constants/works.js
@@ -5,7 +5,7 @@ const works = [
     src: '/talkative_daisy/talkative_daisy.png',
     alt: 'Talkative Daisy',
     intro: {
-      en: 'Talkative Daisy - Urban stress detection, generating daisies based on the loudness and speed of your voice',
+      en: 'Talkative Daisy - Urban stress detection',
       'zh-TW': '聒噪小雛菊 - 都市壓力檢測，根據聲音大小、速度生成雛菊',
     },
     tags: ['p5.js', 'Matter.js', 'ML5'],
