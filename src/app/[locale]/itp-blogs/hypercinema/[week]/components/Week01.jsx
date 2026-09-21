8dac8225a95cdb48419838b8ad4d4cc6096fef24
黃靖軒 <chinghsuan@10-20-74-188.dynapool.wireless.nyu.edu>
2026-09-21 17:57:18 -0400

    feat: upgrade hypercinema list name

--- src/app/[locale]/itp-blogs/hypercinema/[week]/components/Week01.jsx
+++ src/app/[locale]/itp-blogs/hypercinema/[week]/components/Week01.jsx
@@ -10,26 +10,28 @@ const audios = [
 
 const Week01 = () => {
   return (
-    <div>
-      <h3>Week 01</h3>
-      <p>
+    <main className="mx-auto w-full max-w-4xl p-4">
+      <h3 className="text-2xl font-bold">Week 01: Turns Out, I have Ears</h3>
+      <p className="mt-4">
         We were introduced to some basic audio recording equipment and given a few sounds to capture around us. More
-        accurately, they were different **feelings**, such as coldness, counting, comfort, and jealousy. For the feeling
-        of jealousy, we were really hungry, so we recorded the sound of someone else eating and chewing. Walking around
-        with a microphone, looking at the data, and wearing headphones, we were using our **ears to observe the
-        environment instead of our eyes**. I found that experience pretty interesting.
+        accurately, they were different feelings, such as coldness, counting, comfort, and jealousy. For the feeling of
+        jealousy, we were really hungry, so we recorded the sound of someone else eating and chewing.
+      </p>
+      <p className="mt-4">
+        Walking around with a microphone, looking at the data, and wearing headphones, we were using our ears to observe
+        the environment instead of our eyes. I found that experience pretty interesting.
       </p>
 
       {audios.map((audio, index) => (
-        <div key={index}>
-          <p>{audio}</p>
+        <div className="mt-8" key={index}>
+          <p className="text-gray-400">{audio}</p>
           <audio controls preload="metadata">
             <source src={`/audio/hypercinema/week_01/${audio}.wav`} type="audio/mpeg" />
             Cannot play audio
           </audio>
         </div>
       ))}
-    </div>
+    </main>
   );
 };
 
--- src/app/[locale]/itp-blogs/page.jsx
+++ src/app/[locale]/itp-blogs/page.jsx
@@ -3,16 +3,16 @@ import { Link } from '@/i18n/routing';
 import PageTitle from '@/components/PageTitle';
 
 const hypercinemaWeeks = [
-  'week-01',
-  'week-02',
-  'week-03',
-  'week-04',
-  'week-05',
-  'week-06',
-  'week-07',
-  'week-08',
-  'week-09',
-  'week-10',
+  { week: 'week-01', title: 'Turns Out, I Have Ears' },
+  { week: 'week-02', title: '' },
+  { week: 'week-03', title: '' },
+  { week: 'week-04', title: '' },
+  { week: 'week-05', title: '' },
+  { week: 'week-06', title: '' },
+  { week: 'week-07', title: '' },
+  { week: 'week-08', title: '' },
+  { week: 'week-09', title: '' },
+  { week: 'week-10', title: '' },
 ];
 const pcomWeeks = [
   {
@@ -64,10 +64,10 @@ export default function ItpBlogsPage() {
       <section className="mx-auto w-full max-w-3xl py-8">
         <h3 className="text-2xl font-bold">Hypercinema</h3>
         <ul className="mt-4 list-disc pl-6">
-          {hypercinemaWeeks.map((week) => (
+          {hypercinemaWeeks.map(({ week, title }) => (
             <li key={week}>
               <Link href={`/itp-blogs/hypercinema/${week}`} className="text-blue-600">
-                {week}
+                {week} {title && `${title}`}
               </Link>
             </li>
           ))}
