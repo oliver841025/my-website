4616c869ed0986ad0f63d919c9838e8349376a18
黃靖軒 <chinghsuan@10-20-74-188.dynapool.wireless.nyu.edu>
2026-09-22 19:06:17 -0400

    feat: add hypercinema week 02 reflection

--- src/app/[locale]/itp-blogs/hypercinema/[week]/components/Week02.jsx
+++ src/app/[locale]/itp-blogs/hypercinema/[week]/components/Week02.jsx
@@ -14,10 +14,26 @@ const Week02 = () => {
         missing it. I chose this sound partly because it is something I experience myself quite often. Overall, I
         didn&apos;t run into too many problems during the process.
       </p>
-
       <div className="mt-8 flex justify-start gap-8 [&>figure]:mx-0">
         <PhotoFigure src="/hypercinema/week_02/sound_hunter.PNG" alt="First LED" caption="Ready to go" />
         <VideoFigure src="/hypercinema/week_02/recording_test.MOV" caption="Recording test" />
+      </div>
+      <h3 className="mt-6 mb-4 text-2xl font-bold">A guy who try to catch the subway</h3>
+      <p>
+        Our audio journey tells the story of someone trying to catch a subway. We use narrative devices like a ticking
+        noise. We use recordings from the city to signal physical location: honking car for street level, turnstile for
+        entering the subway, noise of subway entrance Our audio journey shows the growing intensity as time counts down.
+        The heart beats faster, the breathing becomes more layered, and the ticking becomes denser. We also have the
+        battery charge dying sound, which adds to the stress of the situation. The ending is left somewhat ambiguous,
+        culminating with the sound of the train leaving. Did the person catch the subway or not? It's supposed to be a
+        tension build and then release.
+      </p>
+
+      <div className="mt-8">
+        <audio controls preload="metadata">
+          <source src={`/hypercinema/week_02/mos.wav`} type="audio/mpeg" />
+          Cannot play audio
+        </audio>
       </div>
     </main>
   );
---
+++ src/app/[locale]/itp-blogs/hypercinema/[week]/components/Week03.jsx
@@ -0,0 +1,11 @@
+import VideoFigure from '@/components/VideoFigure'
+
+const Week03 = () => {
+  return (
+    <main className="mx-auto w-full max-w-4xl p-4">
+        
+    </main>
+  )
+}
+
+export default Week03
